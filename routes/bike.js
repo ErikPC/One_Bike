79887b196a668c48758cfefc7cae2b7797f21e66
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-04 16:52:40 +0100

    feat: crear ruta delete  bici

--- routes/bike.js
+++ routes/bike.js
@@ -7,5 +7,6 @@ const api = express.Router();
 api.post("/bike", BikeController.createBike);
 api.get("/bike", BikeController.getBike);
 api.put("/bike/:name", BikeController.updateBike);
+api.delete("/bike/:name", BikeController.deleteBike);
 
 module.exports = api;
