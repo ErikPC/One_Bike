14c33977c49683bf8c77ff778b01467f855d25eb
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-04 16:46:24 +0100

    feat: crear ruta get devuelve todas bicicletas

--- routes/bike.js
+++ routes/bike.js
@@ -5,5 +5,6 @@ const BikeController = require("../controllers/bike");
 const api = express.Router();
 
 api.post("/bike", BikeController.createBike);
+api.get("/bike", BikeController.getBike);
 
 module.exports = api;
