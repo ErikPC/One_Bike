21244587bb586a7d9bae2f8f6d5ac7fa47773868
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-04 16:43:49 +0100

    feat: crear getBike

--- controllers/bike.js
+++ controllers/bike.js
@@ -16,6 +16,20 @@ async function createBike(req, res) {
   }
 }
 
+async function getBike(req, res) {
+  try {
+    var bikes = await Bike.find();
+    if (!bikes) {
+      res.status(400).send({ msg: "Error al buscar bicicleta" });
+    } else {
+      res.status(200).send(bikes);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
 module.exports = {
   createBike,
+  getBike,
 };
