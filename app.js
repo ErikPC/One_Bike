40eb177d6f47d9ecf5c5034f2f38c786e8180ac7
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-04 16:41:33 +0100

    feat: configurar ruta post

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const bike_routes = require("./routes/bike");
+
+app.use("/api", bike_routes);
+
 module.exports = app;
