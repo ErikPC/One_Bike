0681199a0ea64ca76360ed9428fda25094da4f32
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-12 10:53:38 +0100

    fix: cambiar ruta de bike to store

--- routes/store.js
+++ routes/store.js
@@ -3,6 +3,6 @@ const api = express.Router();
 
 const StoreController = require("../controllers/store");
 
-api.post("/bike", StoreController.createStore);
+api.post("/store", StoreController.createStore);
 
 module.exports = api;
