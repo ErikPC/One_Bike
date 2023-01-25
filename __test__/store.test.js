f5d437250ae04cf4c8bad59455546baa10693c04
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-25 16:46:19 +0100

    test: crear y fixear test store y bike

--- __test__/store.test.js
+++ __test__/store.test.js
@@ -6,32 +6,32 @@ const mongoose = require("mongoose");
 const Store = require("../models/store");
 
 const stores = require("../db/store/store.json");
+const storeToPut = require("../db/store/storeToPut.json");
 const updateStore = require("../db/store/updateStore.json");
 
+jest.setTimeout(10000);
+beforeAll(async () => {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await Store.insertMany(stores);
+});
+
+afterAll(async () => {
+  // Borramos los datos de la base de datos antes de cada prueba
+  await Store.deleteMany({});
+  // Desconectamos de la base de datos de pruebas
+  await mongoose.connection.close();
+});
 describe("Test CRUD", () => {
-  jest.setTimeout(10000);
-  beforeAll(async () => {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await Store.insertMany(stores);
+  test("create a store", async () => {
+    const response = await request(app).post("/api/store").send(updateStore);
+    const data = JSON.parse(response.text).store;
+    expect(data.name).toBe("Mi tienda");
+    expect(response.statusCode).toBe(200);
   });
-
-  afterAll(async () => {
-    // Borramos los datos de la base de datos antes de cada prueba
-    await Store.deleteMany({});
-    // Desconectamos de la base de datos de pruebas
-    await mongoose.connection.close();
-  });
-
-  // test("create a store", async () => {
-  //   const response = await request(app).post("/api/store").send(updateStore);
-  //   const data = JSON.parse(response.text).store;
-  //   expect(data.name).toBe("Mi tienda");
-  //   expect(response.statusCode).toBe(200);
-  // });
 
   test("get all stores", async () => {
     const response = await request(app).get("/api/store");
