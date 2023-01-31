7c2383ad7f7b86af629372a46ecbf2866662e4f6
ErikPC <epardillo@cifpfbmoll.eu>
2023-01-31 17:35:12 +0100

    feat: buscar en BBDD y cambiar disponnibilidad a false

--- repository/repositoryAlquilar.js
+++ repository/repositoryAlquilar.js
@@ -1,7 +1,12 @@
 const Bike = require("../models/bike");
 
 function alquilarBikebyStore(nameStore, nameBike) {
-  return Bike.find({});
+  const bike = Bike.find({ nameStore, nameBike, disponibilidad: true });
+  Bike.updateOne(
+    { nameStore, nameBike, disponibilidad: true },
+    { disponibilidad: false }
+  );
+  return bike;
 }
 
 module.exports = {
